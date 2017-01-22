b01dc49228663766ef963b528c231f0cdc0744d8
DannyLocke <dlocke@mac.com>
2017-01-22 13:00:18 -0600

    updated Main

--- src/com/ironyard/Main.java
+++ src/com/ironyard/Main.java
@@ -7,19 +7,29 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static final int SIZE = 10;
+    public static boolean firstPath = true;
+    public static boolean firstStop = true;
+
+    public static Random r = new Random();
+
+    public static int startingX = r.nextInt(SIZE);
+    public static int startingY = r.nextInt(SIZE);
+
 
     public static ArrayList<ArrayList<Room>> createRooms(){
+        //Room[][] rooms = new Room[SIZE][SIZE];
         ArrayList<ArrayList<Room>> rooms = new ArrayList<>();
 
         for(int row = 0; row < SIZE; row++){
+
             ArrayList<Room> roomRow = new ArrayList<>();
+
             for(int col=0; col<SIZE; col++){
+                //rooms[row][col] = new Room(row,col);
                 roomRow.add(new Room(row,col));
             }
             rooms.add(roomRow);
         }
-
-
         return rooms;
     }//end createRooms()
 
@@ -48,6 +58,11 @@ public class Main {
             Random r = new Random();
             int index = r.nextInt(neighbors.size());
             return neighbors.get(index);
+        } else {
+            if (firstStop == true){
+                //rooms[row][col].setIsEnd(true);
+                firstStop = false;
+            }
         }
         return null;
     }//end randomNeighbor()
@@ -73,7 +88,9 @@ public class Main {
 
     //RECURSIVE METHOD (calling the method in that method)
     public static boolean createMaze(ArrayList<ArrayList<Room>> rooms, Room room) {
+
         room.wasVisited = true;
+
         Room nextRoom = randomNeighbor(rooms, room.row, room.col);
         if (nextRoom == null) {
             return false; //end the method
@@ -81,34 +98,48 @@ public class Main {
 
         tearDownWall(room, nextRoom);
 
-        while(createMaze(rooms, nextRoom)); //new maze with wall "torn down" (minus 1 wall)
+        //new maze with wall "torn down" (minus 1 wall)
+        while(createMaze(rooms, nextRoom)) {
+        }
+        if(firstPath == false){
+            //rooms[startingX][startingY].setIsStart(true);
+            firstPath = true;
+        }
 
         return true;
     }//end createMaze()
 
     public static void main(String[] args) {
+
         ArrayList<ArrayList<Room>> rooms = createRooms();
+
+        System.out.println("Starting index positions: " + startingX + ":" + startingY);
+        System.out.println("The starting and ending position lack Underscores.");
+        //rooms[startingX][startingY].setIsStart(true);
+
         createMaze(rooms, rooms.get(0).get(0));
+
         for(ArrayList<Room> row : rooms) {
             System.out.print(" _"); //prints top line
         }
 
         System.out.println();
+
         for(ArrayList<Room> row : rooms) {
             System.out.print("|"); //prints left "wall"
 
             for(Room room : row) {
-                if(room.hasBottom){
-                    System.out.print("_"); //draws bottom
+                if (room.oStart) {
+                    System.out.print(room.oStart ? "O" : "");
+                    System.out.print(room.hasRight ? "|" : " ");
+                } else if (room.xEnd) {
+                    System.out.print(room.xEnd ? "X" : "");
+                    System.out.print(room.hasRight ? "|" : " ");
 
-                    } else {
-                    System.out.print(" "); //no bottom, a space
-                }
-
-                if (room.hasRight){
-                    System.out.print("|"); //have a right wall?  draws wall
                 } else {
-                    System.out.print(" "); // no wall, prints a space
+                    System.out.print(room.hasBottom ? "_" : " ");  //inline conditional - if the room has a bottom print one otherwise print a space
+                    System.out.print(room.hasRight ? "|" : " ");
+
                 }
             }
             System.out.println();
--- src/com/ironyard/Room.java
+++ src/com/ironyard/Room.java
@@ -11,8 +11,86 @@ public class Room {
     boolean hasBottom = true;
     boolean hasRight = true;
 
+    //starting and ending positions
+    String start;
+    String end;
+    boolean oStart = false;
+    boolean xEnd = false;
+
     public Room (int row, int col){
         this.row = row;
         this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public boolean isWasVisited() {
+        return wasVisited;
+    }
+
+    public void setWasVisited(boolean wasVisited) {
+        this.wasVisited = wasVisited;
+    }
+
+    public boolean isHasBottom() {
+        return hasBottom;
+    }
+
+    public void setHasBottom(boolean hasBottom) {
+        this.hasBottom = hasBottom;
+    }
+
+    public boolean isHasRight() {
+        return hasRight;
+    }
+
+    public void setHasRight(boolean hasRight) {
+        this.hasRight = hasRight;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public boolean isoStart() {
+        return oStart;
+    }
+
+    public void setoStart(boolean oStart) {
+        this.oStart = oStart;
+    }
+
+    public boolean isxEnd() {
+        return xEnd;
+    }
+
+    public void setxEnd(boolean xEnd) {
+        this.xEnd = xEnd;
     }
 }
